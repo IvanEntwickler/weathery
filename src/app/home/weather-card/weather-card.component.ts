37f44780e62088c8bf5fb72be929ed7ca1e779c9
IvanEntwickler <53708215+IvanEntwickler@users.noreply.github.com>
2020-05-18 23:39:02 +0200

    worked on weather-card component

--- src/app/home/weather-card/weather-card.component.ts
+++ src/app/home/weather-card/weather-card.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather-card.component.scss']
 })
 export class WeatherCardComponent implements OnInit {
+  cityName = 'Berlin';
+  condition = 'Sunny';
+  currentTemp = 20;
+  minTemp = 13;
+  maxTemp = 28;
+
 
   constructor() { }
 
