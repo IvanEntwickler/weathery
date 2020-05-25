a7c7bbac26b0f9a176a88021c039f9a5c23beaca
IvanEntwickler <53708215+IvanEntwickler@users.noreply.github.com>
2020-05-25 21:26:06 +0200

    applied functionality to WeatherService and Details Component and connected to Weather API

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,27 +1,117 @@
+import { WeatherService } from './../weather.service';
 import { DaysModel } from './../shared/days.model';
 import { DetailsModel } from './../shared/details.model';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnDestroy {
   details: DetailsModel;
   days: DaysModel;
 
+  city: string;
   today: string;
 
-  constructor() { }
+  stateSubscribtion: Subscription;
+  tempSubscribtion: Subscription;
+  humiditySubscribtion: Subscription;
+  windSubscribtion: Subscription;
+  forecastSubscribtion: Subscription;
 
-  ngOnInit(): void {
+  constructor(private activeRoute: ActivatedRoute, private weatherService: WeatherService) { }
+
+  ngOnInit() {
     //// getting the actual date string
     const todayNumberInWeek = new Date().getDay();
     const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     this.today = days[todayNumberInWeek];
-    console.log(this.today);
+
+    this.activeRoute.paramMap.subscribe((route: any) => {
+      this.city = route.params.city;
+      /// subcribing to state
+      this.stateSubscribtion = this.weatherService.getWeatherState(this.city)
+      .subscribe((state) => this.details.state = state);
+      /// subcribing to temp
+      this.tempSubscribtion = this.weatherService.getCurrentTemp(this.city)
+      .subscribe((temp) => this.details.temp = temp);
+      /// subscribing to hum
+      this.humiditySubscribtion = this.weatherService.getCurrentHum(this.city)
+      .subscribe((hum) => this.details.hum = hum);
+      /// subscribing to wind
+      this.windSubscribtion = this.weatherService.getCurrentWind(this.city)
+      .subscribe((wind) => this.details.wind = wind);
+
+      /// subscribing to the forecast data
+      this.forecastSubscribtion = this.weatherService.getForecast(this.city)
+      .subscribe((data: any) => {
+        console.log(data);
+        // tslint:disable-next-line:prefer-const
+        for (let i of data) {
+          const date = new Date(data[i].dt_txt).getDay();
+          console.log(days[date]);
+          switch (date) {
+            case 0:
+              /// Sunday
+              this.days.dayName7 = days[date];
+              this.days.dayState7 = data[i].weather[0].main;
+              this.days.dayTemp7 = Math.round(data[i].main.temp);
+              break;
+            case 1:
+              /// Monday
+              this.days.dayName1 = days[date];
+              this.days.dayState1 = data[i].weather[0].main;
+              this.days.dayTemp1 = Math.round(data[i].main.temp);
+              break;
+            case 2:
+              /// Tuesday
+              this.days.dayName2 = days[date];
+              this.days.dayState2 = data[i].weather[0].main;
+              this.days.dayTemp2 = Math.round(data[i].main.temp);
+              break;
+            case 3:
+              /// Wednesday
+              this.days.dayName3 = days[date];
+              this.days.dayState3 = data[i].weather[0].main;
+              this.days.dayTemp3 = Math.round(data[i].main.temp);
+              break;
+            case 4:
+              /// Thursday
+              this.days.dayName4 = days[date];
+              this.days.dayState4 = data[i].weather[0].main;
+              this.days.dayTemp4 = Math.round(data[i].main.temp);
+              break;
+            case 5:
+              /// Friday
+              this.days.dayName5 = days[date];
+              this.days.dayState5 = data[i].weather[0].main;
+              this.days.dayTemp5 = Math.round(data[i].main.temp);
+              break;
+            case 6:
+              /// Saturday
+              this.days.dayName6 = days[date];
+              this.days.dayState6 = data[i].weather[0].main;
+              this.days.dayTemp6 = Math.round(data[i].main.temp);
+          }
+        }
+      });
+    });
+
+
+  }
+
+  ngOnDestroy() {
+    this.tempSubscribtion.unsubscribe();
+    this.stateSubscribtion.unsubscribe();
+    this.forecastSubscribtion.unsubscribe();
+    this.humiditySubscribtion.unsubscribe();
+    this.windSubscribtion.unsubscribe();
+
   }
 
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,11 +1,129 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+cityNameSubject = new Subject<string>();
+stateSubject = new Subject<string>();
+citiesSubject = new Subject<any>();
+tempSubject = new Subject<number>();
+humSubject = new Subject<number>();
+maxTempSubject = new Subject<number>();
+minTempSubject = new Subject<number>();
+forecasteSubject = new Subject<any[]>();
 
+constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) { }
+/// calling API for single city data and subcribe
+getCityWeatherByName(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<string> {
+  this.http.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`
+    ).subscribe((nameData) => {
+      // tslint:disable-next-line:no-string-literal
+      this.cityNameSubject.next(nameData['weather']);
+    }, err => {
+      console.log(err);
+    });
+  return this.cityNameSubject;
+}
+
+/// calling API weather data for each city
+getCitiesWeatherArray(cities: string[], metric: 'metric' | 'imperial' = 'metric'): Subject<any> {
+
+  cities.forEach((city) => {
+    this.citiesSubject.next(this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`));
+  });
+  return this.citiesSubject;
+}
+
+/// calling API to get the state of the weather and subscribe to the stateData
+getWeatherState(city: string): Subject<string> {
+  this.http.get(
+  `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+  .subscribe( stateData => {
+  // tslint:disable-next-line:no-string-literal
+  this.stateSubject.next(stateData['weather'][0].main);
+  });
+  return this.stateSubject;
+}
+
+/// calling API to get the current Temperature rounding it to and converting it to a number
+getCurrentTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number> {
+  this.http.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+    .subscribe((weather: any) => {
+      this.tempSubject.next(Math.round(+(weather.main.temp)));
+    });
+  return this.tempSubject;
+}
+
+/// calling API to get current Humidity
+getCurrentHum(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number> {
+  this.http.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+    .subscribe((weather: any) => {
+      this.humSubject.next(+(weather.main.humidity));
+    });
+  return this.humSubject;
+}
+
+/// calling API to get current Wind
+getCurrentWind(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number> {
+  this.http.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+    .subscribe((weather: any) => {
+      this.humSubject.next(Math.floor(+(weather.main.wind)));
+    });
+  return this.humSubject;
+}
+
+/// calling API to get the temp and loop through them and evaluate the maxTemp
+getMaxTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number> {
+  let maxTemp: number;
+
+  this.http.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+    .subscribe((weather: any) => {
+      maxTemp = weather.list[0].main.temp;
+      weather.list.forEach( value => {
+        return maxTemp < value.main.temp ?
+        maxTemp = value.main.temp
+        : value.main.temp;
+      });
+      this.maxTempSubject.next(Math.round(maxTemp));
+    });
+  return this.maxTempSubject;
+}
+
+/// calling API to get the temp and loop through them and evaluate the minTemp
+getMinTemp(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<number> {
+  let minTemp: number;
+
+  this.http.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+    .subscribe((weather: any) => {
+      minTemp = weather.list[0].main.temp;
+      weather.list.forEach( value => {
+        return minTemp > value.main.temp ?
+        minTemp = value.main.temp
+        : value.main.temp;
+      });
+      this.minTempSubject.next(Math.round(minTemp));
+    });
+  return this.minTempSubject;
+}
+
+/// calling API to get the whole Forecast Array for the 5 upcoming days
+getForecast(city: string, metric: 'metric' | 'imperial' = 'metric'): Subject<any[]> {
+  this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${metric}&APPID=0b29194dd2d8dad338f79c617e40d853`)
+  .subscribe((weather: any) => {
+    this.forecasteSubject.next(weather.list);
+  });
+  return this.forecasteSubject;
+}
+
 }
